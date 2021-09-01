927ef6f5b29806d9a9219015077109a37881ab81
Divanshu.Verma <dcdivanshu@gmail.com>
2021-09-01 23:38:23 +0530

    Part 3: Failing test case

---
+++ src/main/java/com/app/ItemService.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import java.util.List;
+
+public class ItemService {
+
+    public Integer getTotalOrderAmount(List<String> items) {
+        // Method to be implemented.
+        return null;
+    }
+
+}
---
+++ src/test/java/ItemServiceTest.java
@@ -0,0 +1,20 @@
+import com.app.ItemService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemServiceTest {
+
+    ItemService itemService = new ItemService();
+    @Test
+    public void check_if_order_amount_is_correct_on_adding_items_to_cart() {
+        List<String> itemsList = new ArrayList<>();
+        itemsList.add("Sweet corn soup");
+        itemsList.add("Vegetable lasagne");
+        Integer total = itemService.getTotalOrderAmount(itemsList);
+        Assertions.assertEquals(388, total);
+    }
+
+}
